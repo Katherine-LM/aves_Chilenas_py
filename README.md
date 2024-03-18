c7229809128aa8460c65b5a7042b4724df9ff248
Katherine-LM <kalm8915@gmail.com>
2024-03-18 17:03:27 -0300

    agrega readme, y estilos a las cards.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Conocimientos de API respetando las buenas prácticas de modularización, utilizando Python.
+
+Aplicación sencilla en el cual se pueden observar algunas imágenes de pájaros típicos de Chile junto con su nombre en español e 
+inglés. 
+
+
+Usando la API 'https://aves.ninjas.cl/api/birds', la cual da acceso a una base de datos con la información requerida.
--- template_api.py
+++ template_api.py
@@ -4,27 +4,32 @@ from string import Template
 url = "https://aves.ninjas.cl/api/birds"
 response = request_json(url)
 
+# Obtener lista de imágenes, título y descripción
+lista_img = [(elemento.get('images', {}).get('main', ''), elemento.get('uid', ''), elemento.get('name', {}).get('spanish', ''), elemento.get('name', {}).get('english', '')) for elemento in response]
 
-lista_img = [(elemento.get('images', {}).get('main', ''), elemento.get('uid', ''), elemento.get('name', {}).get('spanish', '')) for elemento in response]
-
-card =  """<div class="card" style="width: 18rem;">
-                <img src="$url" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">$uid</h5>
-                    <p class="card-text">$name</p>
+# Plantilla para cada tarjeta de ave
+card =  """<div class="col-md-4 mb-3">
+                <div class="card bird-card">
+                    <div class="card-img-container">
+                        <img src="$url" class="card-img-top mx-auto d-block h-100" alt="...">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">$spanish_name</h5>
+                        <h6 class="card-subtitle mb-2 text-muted text-center">$english_name</h6>
+                    </div>
                 </div>
             </div>
     """
 
+# Crear una instancia de Template
 img_template = Template(card)
+
+# Construir el cuerpo de la página con las tarjetas
 texto_img = ''
-for img, title, description in lista_img:
-    texto_img += img_template.substitute(url = img, uid = title, name= description) + '\n'
+for img, uid, spanish_name, english_name in lista_img:
+    texto_img += img_template.substitute(url=img, spanish_name=spanish_name, english_name=english_name) + '\n'
 
-# print(texto_img)
-
-
-# print(imagen)
+# Plantilla HTML
 html_template = Template('''<!doctype html>
 <html lang="en">
   <head>
@@ -32,6 +37,26 @@ html_template = Template('''<!doctype html>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aves Chile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #999;
+        }
+        h1 {
+            padding : 20px;
+        }
+        .bird-card {
+            transition: background-color 0.3s ease;
+        }
+
+        .bird-card:hover {
+            background-color:  rgb(134, 170, 183);
+        }
+
+        .card-img-container {
+            height: 200px;
+            overflow: hidden;
+        }
+    </style>
   </head>
   <body>
     <h1 class="text-center">Aves Chilenas</h1>
@@ -45,8 +70,9 @@ html_template = Template('''<!doctype html>
 </html>
 ''')
 
-html = html_template.substitute(body = texto_img)
+# Rellenar la plantilla HTML con el cuerpo de la página
+html = html_template.substitute(body=texto_img)
 
-archivo = open('index.html', 'w+', encoding='utf-8') 
-archivo.write(html)
-archivo.close()
+# Guardar el archivo HTML
+with open('index.html', 'w+', encoding='utf-8') as archivo:
+    archivo.write(html)
